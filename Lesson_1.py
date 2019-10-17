22c49ba55e082a2f47ec8ebd8ee1b4da90a3deed
Сергей <SergeyDolin@mail.ru>
2019-10-17 18:05:38 +0700

    Lessons 1 and 2

---
+++ Lesson_1.py
@@ -0,0 +1,85 @@
+# Вывод данных
+# print("Sergei Dolin")# Написание текста
+# print('o----') # любые символы
+# print(' ||||') # учитывание пробелов
+# print('*' * 10) # мультипечать
+# _______________________________________
+# Переменные
+# price = 10 # очерёдность объявления переменных
+# price = 20
+# rating = 4.9
+# name = 'Sergei'
+# is_published = False # or True
+# print(price)
+# ________________________________________
+# Задачка
+# full_name = 'John Smith'
+# age = 20
+# is_new = True
+# _________________________________________
+# Ввод данных
+# name = input('What is your name? ')
+# print('Hi ' + name)
+# _________________________________________
+# Задачка
+# name = input('What is your name? ')
+# favorite_color = input('What is your favorite color? ')
+# print(name + ' likes ' + favorite_color)
+# _________________________________________
+# Код с ошибкой
+# birth_year = input('Birth year: ')
+# age = 2019 - birth_year
+# print(age)
+# __________________________________________
+# Конвертация типа строка в целочисленный
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+# ___________________________________________
+# Задачка
+# weigth_kg = input('Weigth (kg): ')
+# weigth_lbs = float(weigth_kg) / 0.45
+# print(weigth_lbs)
+# ___________________________________________
+# course = 'Python for students of 14 gymnasiums'
+# course = "Python's course for students of 14 gymnasiums"
+# course = 'Python for students of "14 gymnasiums"'
+# course = '''
+# Hi Sergei,
+
+# Here is our first email to you.
+
+# Thank you,
+# The support team
+
+# '''
+# course = 'Python for students of 14 gymnasiums'
+#         0123456789........
+# + 1 С начала - 1 С конца
+# 0 or 1 and -1 or -3 and 0:3 or 0: or 5: or :5
+# another = course[:]
+# print(another)
+# _____________________________________________
+# Устная задачка
+# name = 'Jennifer'
+# print(name[1:-1])
+# _____________________________________________
+# Вызов переменных в тексте
+# first = 'Sergei'
+# last = 'Dolin'
+# message = first + ' [' + last + '] is a engineer'
+# msg = f'{first} [{last}] is a engineer'
+# print(msg)
+# _______________________________________________
+# Работа со строками
+# course = 'Python for students of 14 gymnasiums'
+# print(len(course))
+# print(course.upper())
+# print(course)
+# print(course.lower())
+# print((course.find('for')))
+# print(course.replace('for', 'from')) # Соблюдение регистра
+# print('python' in course)
+# ________________________________________________
---
+++ Lesson_2.py
@@ -0,0 +1,61 @@
+# Математические операции
+# print(10 + 3)
+# print(10 / 3)
+# print(10 // 3)
+# print(10 % 3)
+# print(10 ** 3)
+# x = 10
+# x = x + 3
+# x += 3
+# print(x)
+# x -= 3
+# print(x)
+# ___________________________________
+# x = 10 + 3 * 2
+# x = 10 + 3 * 2 ** 2
+# x = (10 + 3) * 2 ** 2
+# print(x)
+# x = (2 + 3) * 10 - 3
+# print(x)
+# ___________________________________
+# import math
+# print(math.ceil(2.9))
+# print(math.floor(2.9))
+# x = 2.9
+# print(round(x))
+# print(abs(-2.9))
+#____________________________________
+# is_hot = False
+# is_cold = False
+#
+# if is_hot:
+#   print("It's a hot day")
+#   print("Drink plenty of water")
+# elif is_cold:
+#    print("It's a cold day")
+#    print("Wear warm clothes")
+# else:
+#    print("It's a lovely day")
+# print("Enjoy your day")
+# ____________________________________
+# Задачка
+# price = 1000000
+# has_good_credit = False
+#
+# if has_good_credit:
+#    down_payment = 0.1 * price
+# else:
+#     down_payment = 0.2 * price
+# print(f"Down payment: ${down_payment}")
+# _____________________________________
+# Логические операции
+# has_high_income = False
+# has_good_income = True
+# has_criminal_record = False
+#
+# if has_high_income and has_good_income:
+#     print("Eligible for loan")
+#if has_high_income or has_good_income:
+#    print("Eligible for loan")
+#if has_good_income and not has_criminal_record:
+#    print("Eligible for loan")
